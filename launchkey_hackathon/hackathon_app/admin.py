d32a199bfb8aef330ba6e58218be7c79d37e557c
cesarmarroquin <cesarm2333@gmail.com>
2015-11-07 11:40:46 -0800

    models laid out, admin setup,  this is the app before initial migration

---
+++ launchkey_hackathon/hackathon_app/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from hackathon_app.models import MainPage, Post, Comment, PostUpvote, CommentUpvote
+
+
+# Register your models here.
+@admin.register(MainPage)
+class MainPageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'creation_date_time')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+    'user', 'mainpage_rel', 'title', 'description', 'url', 'slug', 'creation_time', 'modification_time', 'is_recent',
+    'num_upvotes', 'karma')
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('comment_text', 'user', 'created_time', 'modified_time')
+
+
+@admin.register(PostUpvote)
+class PostUpvoteAdmin(admin.ModelAdmin):
+    list_display = ('post_upvotes', 'up_or_down', 'created_time')
+
+
+@admin.register(CommentUpvote)
+class CommentUpvoteAdmin(admin.ModelAdmin):
+    list_display = ('comment_upvotes', 'up_or_down', 'created_time')
+
+
--- launchkey_hackathon/launchkey_hackathon/settings.py
+++ launchkey_hackathon/launchkey_hackathon/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hackathon_app',
+    'users',
+    'bootstrap3'
 )
 
 MIDDLEWARE_CLASSES = (
