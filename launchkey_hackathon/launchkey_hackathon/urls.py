504c947f9f45963a9198f434c0d53e82d5b8ac8b
Ryan Ransom <Jhonais@Ryans-MacBook-Pro.local>
2015-11-07 16:10:04 -0800

    login potentially done

--- launchkey_hackathon/launchkey_hackathon/urls.py
+++ launchkey_hackathon/launchkey_hackathon/urls.py
@@ -22,6 +22,7 @@ from hackathon_app.views import WelcomePage, ListPosts, CreatePost
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': "/"}, name='logout'),
+    url(r'^login/$', 'users.views.Login', name='login'),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^register/', CreateUser.as_view(), name='register'),
     url(r'^$', WelcomePage.as_view(), name="home" ),
--- launchkey_hackathon/users/views.py
+++ launchkey_hackathon/users/views.py
@@ -1,13 +1,13 @@
-
 from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
+import time
 from users.forms import UserCreateForm
 from django.views.generic import CreateView
 import launchkey
 from launchkey_hackathon.settings import BASE_DIR
 from django.contrib.auth import authenticate, login
-# from django.template.context_processors import request
 import os
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -22,22 +22,28 @@ class CreateUser(CreateView):
 
 
 def Login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    app = '4910033767'
-    secret = '8p20rbc2urf9f9h4wgn6hzarp0wvc7zb'
-    private = open(os.path.join(BASE_DIR, 'secret_key', 'private_key'), 'r').read()
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        if user.is_active:
-            confirm = launchkey.API(app_key=app,private_key=private,app_secret=secret)
-            if confirm:
-                login(request, user)
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        app = '4910033767'
+        secret = '8p20rbc2urf9f9h4wgn6hzarp0wvc7zb'
+        private = open(os.path.join(BASE_DIR, 'secret_key', 'private_key'), 'r').read()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                api = launchkey.API(app_key=app,private_key=private,app_secret=secret)
+                confirm = api.authorize(user)
+                while True:
+                    response = api.poll_request(confirm)
+                    time.sleep(90)
+                    if api.is_authorized(confirm, response['auth']):
+                        login(request, user)
+                        break
+                    else:
+                        pass
             else:
                 pass
         else:
-            pass
+            raise ObjectDoesNotExist("account does not exist")
     else:
-        raise ObjectDoesNotExist("account does not exist")
-
-
+        return render(request, template_name='registration/login.html')
